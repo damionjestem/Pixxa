0f1a228fed8e5eda216b4b1d70796936444dd582
damionjestem <ndnd@protonmail.ch>
2019-02-05 21:01:07 +0100

    Create menu functions

---
+++ src/pixxa/menu.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pixxa;
+
+/**
+ *
+ * @author Damion
+ */
+public class menu {
+
+    public void chooseCheese() {
+        System.out.println("Sery:");
+        System.out.print(
+                "\n1. Żółty ser"
+                + "\n2. Pleśniowy ser"
+                + "\n3. Mozarella"
+                + "\n4. Grana Padano"
+                + "\nESC Cofnij");
+    }
+
+    public void chooseMeat() {
+        System.out.println("Sery:");
+        System.out.print(
+                "\n1. Żółty ser"
+                + "\n2. Pleśniowy ser"
+                + "\n3. Mozarella"
+                + "\n4. Grana Padano"
+                + "\nESC Cofnij");
+    }
+}
