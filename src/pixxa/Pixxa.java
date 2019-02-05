020f55712c3de8708d2714f78277f7838031c3de
damionjestem <ndnd@protonmail.ch>
2019-02-05 21:13:28 +0100

    Assign button usage to a method

--- src/pixxa/Pixxa.java
+++ src/pixxa/Pixxa.java
@@ -1,32 +1,43 @@
 package pixxa;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Pixxa extends Application {
+public class Pixxa extends Application implements EventHandler<ActionEvent> {
 
     Button button;
-    
+
     public static void main(String[] args) {
         launch(args);
     }
-    
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Pixxa - zmixxuj pizzę!");
-        
+
         /*You can add text inside brackets of constructor*/
         button = new Button();
         button.setText("Naciśnij mnie");
-        
+        button.setOnAction(this);
+
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
-        
+
         Scene scene = new Scene(layout, 300, 500); //layout, x, y
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        if (event.getSource()==button) {
+            System.out.println("Tekst testowy, owocowy");
+        }
+        
     }
 }
